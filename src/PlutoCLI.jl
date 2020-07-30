efa4b114b1304a7f049c48ee766d6d801a287a02
Roger-luo <rogerluo.rl18@gmail.com>
2020-07-29 21:15:10 -0400

    init CLI

--- src/PlutoCLI.jl
+++ src/PlutoCLI.jl
@@ -1,5 +1,26 @@
 module PlutoCLI
 
-# Write your package code here.
+using Comonicon, Pluto
+
+# run(host, port::Integer; launchbrowser::Bool=false, session=ServerSession())
+
+"""
+Pluto CLI - Lightweight reactive notebooks for Julia
+
+# Arguments
+
+- `host`: default is 127.0.0.1 (localhost)
+
+# Options
+
+- `-p,--port <int>`: port you want to specify, default is 1234
+
+# Flags
+
+- `-l,--launchbrowser`: add this flag to launch browser.
+"""
+@main function pluto(;host="127.0.0.1", port::Int=1234, launchbrowser::Bool=false)
+    Pluto.run(host, port; launchbrowser=launchbrowser)
+end
 
 end
